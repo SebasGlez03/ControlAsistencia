c4c58e75f56d2ff1a654ad618681c4a01f5ad0d4
SebasGlez03 <sebas.glez18040465@gmail.com>
2024-10-19 16:12:56 -0700

    se termino UsuarioDTO

--- DTOs/src/main/java/dtos/UsuarioDTO.java
+++ DTOs/src/main/java/dtos/UsuarioDTO.java
@@ -9,5 +9,58 @@ package dtos;
  * @author PC
  */
 public class UsuarioDTO {
-    
+
+    protected String nombre;
+    protected String correo;
+    protected Long id;
+    protected String contrasenia;
+
+    public UsuarioDTO() {
+    }
+
+    public UsuarioDTO(String nombre, String correo, Long id, String contrasenia) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.id = id;
+        this.contrasenia = contrasenia;
+    }
+
+    public UsuarioDTO(String nombre, String correo, String contrasenia) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contrasenia = contrasenia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
 }
