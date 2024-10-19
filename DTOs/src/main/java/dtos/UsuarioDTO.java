fffe88934318654ce9d7ef5e741e9ce346250dfc
SebasGlez03 <sebas.glez18040465@gmail.com>
2024-10-19 16:26:27 -0700

    Comentarios de la clase UsuarioDTO

--- DTOs/src/main/java/dtos/UsuarioDTO.java
+++ DTOs/src/main/java/dtos/UsuarioDTO.java
@@ -15,9 +15,20 @@ public class UsuarioDTO {
     protected Long id;
     protected String contrasenia;
 
+    /**
+     * Constructor por omision de la clase
+     */
     public UsuarioDTO() {
     }
 
+    /**
+     * Constructor que inicializa el valor de los parametros de la clase
+     *
+     * @param nombre nombre del usuario
+     * @param correo correo del usuario
+     * @param id id del usuario
+     * @param contrasenia contrasenia del usuario
+     */
     public UsuarioDTO(String nombre, String correo, Long id, String contrasenia) {
         this.nombre = nombre;
         this.correo = correo;
@@ -25,42 +36,100 @@ public class UsuarioDTO {
         this.contrasenia = contrasenia;
     }
 
+    /**
+     * Constructor que inicializa el valor de los parametros de la clase a
+     * excepcion del id
+     *
+     * @param nombre nombre del usuario
+     * @param correo correo del usuario
+     * @param contrasenia contrasenia del usuario
+     */
     public UsuarioDTO(String nombre, String correo, String contrasenia) {
         this.nombre = nombre;
         this.correo = correo;
         this.contrasenia = contrasenia;
     }
 
+    /**
+     * Metodo que obtiene el nombre del usuario
+     *
+     * @return nombre del usuario
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Metodo que establece el nombre del usuario
+     *
+     * @param nombre nombre del usuario
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Metodo que obteiene le correo del usuario
+     *
+     * @return correo del usuario
+     */
     public String getCorreo() {
         return correo;
     }
 
+    /**
+     * Metodo que establece el correo del usuario
+     *
+     * @param correo correo del usuario
+     */
     public void setCorreo(String correo) {
         this.correo = correo;
     }
 
+    /**
+     * Metodo que obtiene el id del usuario
+     *
+     * @return id del usuario
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Metodo que establece el id del usuario
+     *
+     * @param id id del usuario
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Metodo que obtiene la contrasenia
+     *
+     * @return contrasneia del usuario
+     */
     public String getContrasenia() {
         return contrasenia;
     }
 
+    /**
+     * Metodo que establece la contrasenia
+     *
+     * @param contrasenia contrasenia del usuario
+     */
     public void setContrasenia(String contrasenia) {
         this.contrasenia = contrasenia;
+    }
+
+    /**
+     * Metodo que devuelve todos los parametros de la clase en una string
+     *
+     * @return String con los valores de todos los parametros de la clase
+     */
+    @Override
+    public String toString() {
+        return "UsuarioDTO{" + "nombre=" + nombre + ", correo=" + correo + ", id=" + id + ", contrasenia=" + contrasenia + '}';
     }
 
 }
