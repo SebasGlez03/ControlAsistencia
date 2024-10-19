21a142bbbd6c7af13c4fc58659466d50ac82d40c
vvcrxyz <nomar.limon04@gmail.com>
2024-10-19 16:19:37 -0700

    Hice merge

--- DTOs/src/main/java/dtos/AlumnoDTO.java
+++ DTOs/src/main/java/dtos/AlumnoDTO.java
@@ -10,6 +10,31 @@ package dtos;
  */
 public class AlumnoDTO extends UsuarioDTO{
     
-    
-    
+    private int semestre;
+    private float promedio;
+
+    public AlumnoDTO() {
+    }
+
+    public AlumnoDTO(int semestre, float promedio) {
+        this.semestre = semestre;
+        this.promedio = promedio;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+
+    public float getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(float promedio) {
+        this.promedio = promedio;
+    }
+     
 }
