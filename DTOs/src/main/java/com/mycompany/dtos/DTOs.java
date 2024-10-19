9ca9ac65777f1cc73aebc25e7823c1e870bd953e
SebasGlez03 <sebas.glez18040465@gmail.com>
2024-10-19 15:57:28 -0700

    paquete DTOs

---
+++ DTOs/src/main/java/com/mycompany/dtos/DTOs.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dtos;
+
+/**
+ *
+ * @author PC
+ */
+public class DTOs {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
