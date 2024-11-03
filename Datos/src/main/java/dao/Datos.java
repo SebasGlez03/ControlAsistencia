057cec80c16a35d55f815686c114a433ecde5e6d
SebasGlez03 <sebas.glez18040465@gmail.com>
2024-11-03 15:37:44 -0700

    se agregaron los proyectos necesarios

---
+++ Datos/src/main/java/dao/Datos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package dao;
+
+/**
+ *
+ * @author PC
+ */
+public class Datos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
