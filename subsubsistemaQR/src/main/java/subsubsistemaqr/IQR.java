c30c22a26ee2afe771063a25501cb634111cd113
Henry S. <henrysoto.rz@gmail.com>
2024-11-06 00:35:02 -0700

    ADD: Subsistema QR

---
+++ subsubsistemaQR/src/main/java/subsubsistemaqr/IQR.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package subsubsistemaqr;
+
+import entidades.QR;
+
+/**
+ *
+ * @author Ragzard
+ */
+public interface IQR {
+
+    /**
+     * Devuelve un QR generado a partir de un PIN y una Fecha.
+     *
+     * @return
+     */
+    public QR generarQR();
+
+    /**
+     * Genera un nuevo QR validando que no sea igual al qr anterior.
+     *
+     * @param qr
+     * @return
+     */
+    public QR actualizarQR(QR qr);
+
+    /**
+     * Valida que el pin introducido sea igual al del QR.
+     *
+     * @param pin
+     * @param qr
+     * @return true : false
+     */
+    public boolean escanearQR(int pin, QR qr);
+}
