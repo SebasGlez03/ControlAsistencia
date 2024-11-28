d8ff61bc3c169cdb84f2e8133013b7a82f4f99ec
joeljolos-TW <joel.cabe2403@gmail.com>
2024-11-27 23:35:03 -0700

    Subsistema RegistrosClase v0.1
    una mejor idea de lo que habia hecho

--- SubsistemaRegistros/src/main/java/SubsistemaRegistros/Materia.java
+++ SubsistemaRegistros/src/main/java/SubsistemaRegistros/Materia.java
@@ -15,47 +15,16 @@ public class Materia {
     private String nombre;
     private int Semestre;
 
-    public int getSemestre() {
-        return Semestre;
-    }
-
-    public void setSemestre(int Semestre) {
-        this.Semestre = Semestre;
-    }
-    private Maestro maestro;
-    private List<Alumno> alumnosInscritos;
-
     public Materia() {
     }
 
-    public Materia(int ID, String nombre, int Semestre ,Maestro maestro, List<Alumno> alumnosInscritos) {
+    public Materia(int ID, String nombre, int Semestre ) {
         this.ID = ID;
         this.nombre = nombre;
         this.Semestre=Semestre;
-        this.maestro=maestro;
-        this.alumnosInscritos = alumnosInscritos;
+        
     }
     
-    
-    
-    public void InscribirAlumno(Alumno alumno)throws IllegalArgumentException{
-        try {            
-           alumnosInscritos.add(alumno); 
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("El alumno ya se encuentra inscrito en la clase");
-        }
-    }
-    
-    public void BajaAlumno(Alumno alumno)
-    {
-        if(!alumnosInscritos.contains(alumno)){
-            throw new IllegalArgumentException("el alumno no se encuentra inscrito");
-        }
-        alumnosInscritos.remove(alumno);
-    }
-    public List<Alumno> listarAlumnos(){
-        return alumnosInscritos;
-    }
 
     public int getID() {
         return ID;
@@ -72,27 +41,15 @@ public class Materia {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
-    public Maestro getMaestro() {
-        return maestro;
-    }
-
-    public void setMaestro(Maestro maestro) {
-        this.maestro = maestro;
-    }
-
-    public List<Alumno> getAlumnosInscritos() {
-        return alumnosInscritos;
-    }
-
-    public void setAlumnosInscritos(List<Alumno> alumnosInscritos) {
-        this.alumnosInscritos = alumnosInscritos;
-    }
     
-    public boolean estaLlena(){
-        if(alumnosInscritos.size()==25){
-            return true;
-        }
-        return false;
+    
+    public int getSemestre() {
+        return Semestre;
     }
+
+    public void setSemestre(int Semestre) {
+        this.Semestre = Semestre;
+    }
+
+    
 }
---
+++ SubsistemaRegistros/src/main/java/SubsistemaRegistros/RegistrosClase.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package SubsistemaRegistros;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import entidades.*;
+import java.util.ArrayList;
+import java.util.List;
+import persistencia.FachadaPersistencia;
+import persistencia.IPersistencia;
+import java.util.*;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+/**
+ * 
+ *
+ * @author joel_
+ */
+public class RegistrosClase {
+    
+    
+    private IPersistencia IPer = new FachadaPersistencia();
+    public RegistrosClase() {
+    }
+    
+    
+    public void agregarMateria(Materia materia, Maestro maestro){
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        MongoDatabase database = mongoClient.getDatabase("cia");
+        MongoCollection<Document> collection = database.getCollection("Sesiones");
+        
+        List<Alumno> listaAsistencia = new ArrayList<Alumno>();
+        
+        Materia materiaNueva= new Materia(materia.getID(),
+                materia.getNombre(),
+                materia.getSemestre());
+        
+        Document document = new Document();
+        document.append("_id", new ObjectId());
+        //document.append("fecha", )
+        document.append("id_clase", new ObjectId());
+        document.append("id_maestro", maestro.getMatricula());
+        document.append("listaAsistencia",listaAsistencia);
+        
+        collection.insertOne(document);
+    }
+    
+    
+    
+    public void inscribirAlumnoEnMateria(Alumno alumno, Materia materia){
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        MongoDatabase database = mongoClient.getDatabase("cia");
+        MongoCollection<Document> collection = database.getCollection("Sesiones");
+        
+        collection.find()
+    }
+    
+    public List<Materia> listarMateriasDisponibles(){
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        MongoDatabase database = mongoClient.getDatabase("cia");
+        MongoCollection<Materia> collection = database.getCollection("clases",Materia.class);
+        
+        
+    }
+    /**
+    public List<Alumno> obtenerAlumnosPorMateria(Materia materia){
+        
+    }**/
+    
+    
+}
